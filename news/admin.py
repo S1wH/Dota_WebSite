9c5dff86a8bbf878e38ed3f39e1bde068140435b
S1wH <gomozov.pavel@yandex.ru>
2023-03-05 19:43:52 +0300

    news model added

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from news.models import Author, News
 
 # Register your models here.
+admin.site.register(News)
+admin.site.register(Author)
--- news/models.py
+++ news/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    nickname = models.CharField(max_length=15)
+    first_publish_date = models.DateTimeField(blank=True, null=True)
+    rating = models.FloatField(default=0)
+
+    def __str__(self):
+        return f'{self.nickname}'
+
+
+class News(models.Model):
+    header = models.CharField(max_length=30, unique=True)
+    summary = models.CharField(max_length=100)
+    text = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    publish_date = models.DateTimeField(auto_now_add=True)
+    importance_index = models.BooleanField(default=False)
+    # video = models.FileField(upload_to=)
+
+    def __str__(self):
+        return f'{self.header}'
+
+    class Meta:
+        verbose_name = 'News'
+        verbose_name_plural = 'News'
