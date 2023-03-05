1d8244b939acfa8ebe3eee123df86d31a85dcf8a
DanteOnline <iamdanteonline@gmail.com>
2023-03-05 09:17:46 -0800

    add image example

--- mainapp/models.py
+++ mainapp/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Author(models.Model):
     name = models.CharField(max_length=32)
+    avatar = models.ImageField(upload_to='avatars', blank=True, null=True)
 
     def __str__(self):
         return f'{self.name}'
