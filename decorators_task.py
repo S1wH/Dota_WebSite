99987509d0f5ed99de949d386b8ca4d0d74f9f3d
DanteOnline <iamdanteonline@gmail.com>
2023-04-18 09:08:24 -0700

    add in example

--- decorators_task.py
+++ decorators_task.py
@@ -24,6 +24,12 @@ def leave_first_last(sp: list) -> list:
     return [el for index, el in enumerate(sp) if index == 0 or index == len(sp) - 1]
 
 
+@file_output
+def leave_first_last_range(sp: list) -> list:
+    return [sp[i] for i in range(len(sp)) if i in [0, len(sp) - 1]]
+
 delete_even_elements([1, 2, 3, 4, 5], 'file1.txt')
 delete_odd_elements([1, 2, 3, 4, 5], 'file2.txt')
 leave_first_last([1, 2, 3, 4, 5])
+
+print(leave_first_last_range([2, 3]))
