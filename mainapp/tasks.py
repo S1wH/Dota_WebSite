782fc7e8c8fbb4c6cd17b35a56f66914b2ac5956
S1wH <gomozov.pavel@yandex.ru>
2023-08-15 18:04:14 +0300

    reworked email sending script

--- mainapp/tasks.py
+++ mainapp/tasks.py
@@ -1,17 +1,19 @@
 from __future__ import absolute_import, unicode_literals
 from celery import shared_task
 from celery.utils.log import get_task_logger
-from django.core.mail import send_mail
+from django.core import mail
 
 logger = get_task_logger(__name__)
 
 
 @shared_task
 def send_email(email):
-    logger.info("Sent email")
-    send_mail('Contact from',
-              'Thanks for signing in',
-              'settings.EMAIL_HOST_USER',
-              [email],
-              fail_silently=False,
-              )
+    with mail.get_connection() as connection:
+        logger.info("Sent email")
+        mail.EmailMessage(
+            subject='Contact from',
+            body='Thanks for signing in',
+            from_email='settings.EMAIL_HOST_USER',
+            to=[email],
+            connection=connection
+        ).send()
